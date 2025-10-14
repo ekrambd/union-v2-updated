1d36acc306173de38989f4783fddeffe72e35e62
ekrmabd <ekramhossainekram28@gmail.com>
2025-10-14 11:11:23 +0600

    Curl URL used

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2320,26 +2320,24 @@ class ApiController extends Controller
                 //     $file->move(public_path().'/uploads/users/', $name); 
                 //     $path = public_path('uploads/users/' . $name); // Full path needed
                 // }
-                $curl = curl_init();
-
                 curl_setopt_array($curl, array(
-                    CURLOPT_URL => 'https://union-express-bd.com/rides/api/profile_update.php',
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => '',
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 0,
-                    CURLOPT_FOLLOWLOCATION => true,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => 'POST',
-                    CURLOPT_POSTFIELDS => array(
-                        'user_name' => $user->user_name,
-                        'password' => $request->password,
-                        'image' => new CURLFile($path), // Correct way to send the file
-                    ),
-                ));
+                CURLOPT_URL => 'https://union-express-bd.com/rides/api/profile_update.php',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array(
+                    'user_name' => $user->user_name,
+                    'password' => $request->password,
+                    'image' => new \CURLFile($path),
+                ),
+            ));
 
-                $response = curl_exec($curl);
-                curl_close($curl);
+            $response = curl_exec($curl);
+            curl_close($curl);
 
                 //echo $response;
 
