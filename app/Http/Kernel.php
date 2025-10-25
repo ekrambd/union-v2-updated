e58b2e7bcbbf0e27dd8688045a964203a4c09256
ekrmabd <ekramhossainekram28@gmail.com>
2025-10-25 16:26:30 +0600

    the change

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \App\Http\Middleware\CustomCorsMiddleware::class,
-        \App\Http\Middleware\EnsureEmailIsVerified::class,
+        \App\Http\Middleware\EnsureRequestFromBrowser::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class, 
     ];
 
