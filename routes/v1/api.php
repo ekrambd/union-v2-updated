a4677b9ad99e9a902117ce0cf06b24c6fbaf619a
ekrmabd <ekramhossainekram28@gmail.com>
2025-10-25 18:40:58 +0600

    ffs0

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -154,6 +154,7 @@ Route::middleware('auth:sanctum')->group( function () {
 Route::middleware(['auth:courieragent','from.browser','custom.cors'])->group(function () {
     Route::apiResource('courierriders',CourierriderController::class);
     Route::post('courier-rider-signout', [ApiController::class, 'courierRiderSignout']);
+    Route::post('courier-order-status-update', [ApiController::class, 'courierOrderStatusUpdate']);
 });
 
 
