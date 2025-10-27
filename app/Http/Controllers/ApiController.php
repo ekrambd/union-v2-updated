f9cd5963b2f0bd2ab372792e4171d57fc2472fdf
ekrmabd <ekramhossainekram28@gmail.com>
2025-10-27 14:39:45 +0600

    try catch

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -191,6 +191,26 @@ class ApiController extends Controller
        try
         {  
 
+            $origin = $request->headers->get('Origin') ?? '*';
+
+            // If the request is OPTIONS (preflight), return a quick 200 response
+            if ($request->getMethod() === 'OPTIONS') {
+                return response()->json('OK', 200, [
+                    'Access-Control-Allow-Origin' => $origin,
+                    'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With, Accept, Origin',
+                    'Access-Control-Allow-Credentials' => 'true',
+                ]);
+            }
+
+            // Continue normal flow for other requests
+            $response = $next($request);
+
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, Origin');
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
+
             $validator = Validator::make($request->all(), [
                 'login' => 'required|string',
                 'password' => 'required|string',
