feafb10b1ab41f96ada2c4c0e5e36450ecd5910b
ekrmabd <ekramhossainekram28@gmail.com>
2025-12-07 14:27:08 +0600

    route changes

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 }); 
 
-Route::post('/user/user-password-update', [ApiController::class, 'userPasswordUpdate']);
+Route::post('/user/password-update', [ApiController::class, 'userPasswordUpdate']);
 Route::get('/user/offers', [ApiController::class, 'offers']);
 Route::get('/user/suggestions', [ApiController::class, 'suggestions']);
 Route::get('/user/app-banner-images', [ApiController::class, 'appBannerImages']);
