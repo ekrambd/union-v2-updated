858a343af990afb5c8beca180547411568baa322
ekrmabd <ekramhossainekram28@gmail.com>
2026-05-14 13:39:11 +0600

    chat id define

--- app/Models/Doctorappointment.php
+++ app/Models/Doctorappointment.php
@@ -10,7 +10,7 @@ class Doctorappointment extends Model
     use HasFactory;
     
 
-    protected $appends = ['chat_id'];
+    protected $appends = ['user_chat_id','doctor_chat_id'];
 
     public function doctor()
     {
@@ -37,10 +37,17 @@ class Doctorappointment extends Model
         return $this->hasOne(Doctorconversation::class);
     }
     
-    public function getChatIdAttribute()
+    public function getDoctorChatIdAttribute()
     {
         $getUser = \DB::table('doctors')->where('id',$this->doctor_id)->first();
         $user = \DB::connection('mysql_second')->table('users')->where('phone',$getUser->phone)->where('role','doctor')->first();
+        return $user?strval($user->id):"";
+    }
+
+    public function getUserChatIdAttribute()
+    {
+        $getUser = \DB::table('users')->where('id',$this->user_id)->first();
+        $user = \DB::connection('mysql_second')->table('users')->where('phone',$getUser->mobile)->where('role','user')->first();
         return $user?strval($user->id):"";
     }
 
